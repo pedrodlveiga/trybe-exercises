434eafa22db06a4394432d28616e33d3bf64b458
Pedro Veiga <pedroveiga.ti@gmail.com>
2021-07-21 17:14:37 -0300

    exercicios 02 e 04

--- fundamentos/bloco-5-javascript-dom-eventos-storage/exercises/javascript-dom-eventos-e-web-storage_3/exercises_5_3/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-storage/exercises/javascript-dom-eventos-e-web-storage_3/exercises_5_3/script.js
@@ -14,3 +14,65 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+
+  let dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+function createDaysDecember() {
+   
+       
+    let dezDays = document.querySelector("#days");
+    
+    for(let index = 0; index < dezDaysList.length; index += 1){
+        let dia = dezDaysList[index];
+        let diaLi = document.createElement('li');
+        
+        if (dia === 24 | dia === 31){
+            diaLi.className = ' day feriado';
+            diaLi.innerHTML = dia;
+           dezDays.appendChild(diaLi)
+        } else if (dia === 4 | dia === 11 | dia === 18) {
+          diaLi.className = 'day sexta-feira'
+          diaLiInnerHTML = dia;
+          diaLi.innerHTML = dia;
+          dezDays.appendChild(diaLi)
+        } else if (dia === 25) {
+          diaLi.ClassName = 'day feriado'
+          diaLi.innerHTML = dia;
+          dezDays.appendChild(diaLi)
+        } else {
+        diaLi.ClassName = 'day'
+        diaLi.innerHTML = dia;
+        dezDays.appendChild(diaLi);
+        }
+    }         
+}
+createDaysDecember();
+
+// EXERCÍCIO 02
+
+
+function recebeFeriados(buttonName) {
+
+  let selDivButton = document.getElementsByTagName('div')[2];
+  let criaButton = document.createElement('button');
+  selDivButton.appendChild(criaButton);
+  // criaButton.id = criaButtonID;// opção de criar e salvar em uma variável o nome do ID do elemento button criado e depois declarar que o id = variável criada;
+  criaButton.id = "btn-holiday"; 
+  criaButton.innerHTML = buttonName;
+}
+recebeFeriados('Feriados')
+
+
+
+//EXERCÍCIO 04
+
+function recebeFridays(buttonName) {
+  let selDivButton = document.querySelector('.buttons-container');
+  let criaButton = document.createElement('button');
+  selDivButton.appendChild(criaButton);
+  // criaButton.id = criaButtonID;// opção de criar e salvar em uma variável o nome do ID do elemento button criado e depois declarar que o id = variável criada;
+  criaButton.id = "btn-friday"; 
+  criaButton.innerHTML = buttonName;
+}
+recebeFridays("Sexta-feiras")
+
