06d13fc23b6cc15eb76350c2b6719a10227941da
Pedro Veiga <pedroveiga.ti@gmail.com>
2021-08-20 14:25:29 -0300

    Exercícios de 01 a 05

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/script.js
@@ -1,3 +1,4 @@
+// PARTE 01
 const order = {
     name: 'Rafael Andrade',
     phoneNumber: '11-98763-1416',
@@ -53,3 +54,77 @@ const order = {
   }
   
   orderModifier(order);
+
+  // PARTE 02
+
+  const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+  // Exercício 01
+  
+  const funcOne = (one) => {
+    const turnoManha = {
+      turno: 'manhã',
+    }
+    const insereTurno = Object.assign(lesson2, turnoManha)
+    return insereTurno;
+  }
+  console.table(funcOne());
+
+  // Exercício 02
+
+  const funcTwo = (two) => {
+    const listaKeys = Object.keys(lesson1);
+    return listaKeys;
+  }
+  console.table(funcTwo());
+
+  // Exercício 03
+
+  const funcThree = (three) => {
+    const tamanhoObj = Object.entries(lesson3);
+    return tamanhoObj.length;
+  }
+  console.log(funcThree());
+
+  // Exercício 04
+
+  const funcFour = (four) => {
+    const valorObj = Object.values(lesson1);
+    return valorObj;
+  }
+  console.log(funcFour());
+
+  // Exercício 05
+
+  const allLessons = {};
+
+  Object.assign(allLessons, {lesson1}, {lesson2}, {lesson3});
+  
+
+  console.log(allLessons);
+
+
+  
+
+
+
+
+
